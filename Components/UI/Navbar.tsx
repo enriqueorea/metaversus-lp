70cce445fd26cbd838b7c14c281a6169de875a6a
Enrique Orea <orea_96@hotmail.com>
2022-11-25 19:08:09 -0600

    Navbar UI finalizado

--- Components/UI/Navbar.tsx
+++ Components/UI/Navbar.tsx
@@ -1,13 +1,45 @@
-import React from "react";
+"use client";
+
+import React, { FC } from "react";
 import { motion } from "framer-motion";
 
 import { navVariants } from "../../utils/motion";
 import styles from "../../styles";
+import Image from "next/image";
 
-type Props = {};
+const Navbar: FC = () => {
+  return (
+    <motion.nav
+      initial="hidden"
+      whileInView={"show"}
+      variants={navVariants}
+      className={`${styles.xPaddings} py-8 relative`}
+    >
+      <div className="absolute w-[50%] inset-0 gradient-01" />
+      <div
+        className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}
+      >
+        <Image
+          src="/search.svg"
+          alt="search"
+          width={24}
+          height={24}
+          className="object-contain"
+        />
+        <h2 className="font-extrabold text-[24px] text-white leading-[30px]">
+          Metaverses
+        </h2>
 
-const Navbar = (props: Props) => {
-  return <div>Navbar</div>;
+        <Image
+          src={"/menu.svg"}
+          alt="menu"
+          width={24}
+          height={24}
+          className="object-contain"
+        />
+      </div>
+    </motion.nav>
+  );
 };
 
 export default Navbar;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { Navbar } from "../Components";
+import { Hero } from "../sections";
 
 const Page = () => (
-  <>
+  <div className="bg-primary-black overflow-hidden">
     <Navbar />
-  </>
+    <Hero />
+  </div>
 );
 
 export default Page;
