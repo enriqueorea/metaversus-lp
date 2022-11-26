10deb19e0bfba4126ee0936170ced7f883846cc7
Enrique Orea <orea_96@hotmail.com>
2022-11-25 20:28:33 -0600

    ADD: about section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import { Navbar } from "../Components";
-import { Hero } from "../sections";
+import { Hero, About } from "../sections";
 
 const Page = () => (
   <div className="bg-primary-black overflow-hidden">
     <Navbar />
     <Hero />
+    <About />
   </div>
 );
 
