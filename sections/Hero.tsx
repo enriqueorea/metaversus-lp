aca104c3dcc2d88df436c103021be96e4b312937
Enrique Orea <orea_96@hotmail.com>
2022-11-25 19:48:32 -0600

    DONE: hero section

--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -1,7 +1,62 @@
+"use client";
 import { FC } from "react";
+import Link from "next/link";
+
+import { motion } from "framer-motion";
+
+import { slideIn, staggerContainer, textVariant } from "../utils/motion";
+import styles from "../styles";
 
 const Hero: FC = () => {
-  return <div>Hero</div>;
+  return (
+    <section className={`${styles.yPaddings} sm:pl-16 pl-6`}>
+      {" "}
+      <motion.div
+        variants={staggerContainer}
+        initial="hidden"
+        whileInView={"show"}
+        viewport={{ once: false, amount: 0.25 }}
+        className={`${styles.innerWidth} mx-auto flex flex-col`}
+      >
+        {" "}
+        <div className="flex justify-center items-center flex-col relative z-10">
+          <motion.h1 className={styles.heroHeading} variants={textVariant(1.1)}>
+            Metaverses
+          </motion.h1>
+          <motion.div
+            className="flex flex-row justify-center items-center"
+            variants={textVariant(1.2)}
+          >
+            <h1 className={styles.heroHeading}>MA</h1>
+            <div className={styles.heroDText} />
+            <h1 className={styles.heroHeading}>NESS</h1>
+          </motion.div>
+        </div>
+        <motion.div
+          variants={slideIn("right", "tween", 0.2, 1)}
+          className="relative w-full md:-mt-[20px] -mt-[12px]"
+        >
+          <div className="absolute w-full h-[300px] hero-gradient rounded-tl-[140px] z-0 -top-[30px] " />
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            src="/cover.png"
+            alt="logo"
+            className="w-full sm:h-[500px] h-[350px] object-cover rounded-tl-[140px] z-10 relative"
+          />
+          <Link href={"#explore"}>
+            <div className="w-full flex justify-end sm:-mt-[70px] -mt-[50px] pr-[40px] relative z-10">
+              {/* eslint-disable-next-line @next/next/no-img-element */}
+              <img
+                src={"/stamp.png"}
+                alt={"stamp"}
+                className="sm:w-[155px] w-[100px] sm:h-[155px] h-[100px] object-contain"
+              />
+            </div>
+          </Link>
+        </motion.div>
+      </motion.div>
+    </section>
+  );
 };
 
 export default Hero;
